f22163e6e8d725b7e7952bec08352436a47851cd
LE15-442\Fernando Garcia <fgarcia@na-at.com.mx>
2023-07-20 23:24:14 -0600

    se agregan mas entities

---
+++ src/main/java/com/tt/unitify/modules/admin/AdminEntity.java
@@ -0,0 +1,11 @@
+package com.tt.unitify.modules.admin;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class AdminEntity extends AdminDto{
+    String id;
+
+}
